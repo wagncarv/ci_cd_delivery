b616b517401394f1a281a2a13c4828eee6489849
wagner <wagncarv@gmail.com>
2022-01-13 15:40:39 -0300

    correcoes test

--- lib/rockelivery_web/views/error_view.ex
+++ lib/rockelivery_web/views/error_view.ex
@@ -31,7 +31,7 @@ defmodule RockeliveryWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
   # "Not Found".
-  def template_not_found(template, assigns) do
+  def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
 end
